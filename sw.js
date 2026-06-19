4cccde577eede3e5dc92785b4b04270360115a69
ekoutstaal-netizen <ekoutstaal@gmail.com>
2026-06-20 01:17:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE = 'werkkracht-v1';
+const CACHE = 'wkn-v1';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/icon192.png',
+  '/icon512.png'
 ];
 
 self.addEventListener('install', e => {
@@ -25,6 +25,8 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/index.html'));
+    })
   );
 });
